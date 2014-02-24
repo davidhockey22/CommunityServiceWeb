e8a508cf03a5438f608cf55095615b324e2ddd54
davidhockey22 <davidhockey22ster@gmail.com>
2014-02-24 10:48:16 -0500

    added login servlet for android

---
+++ CommunityService/src/AndroidWebService/LoginServlet.java
@@ -0,0 +1,94 @@
+package AndroidWebService;
+
+import hibernate.HibernateUtil;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.CommunityService.EntitiesMapped.Volunteer;
+import org.CommunityService.Services.VolunteerService;
+import org.CommunityService.Validators.PasswordHash;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/*
+ import com.google.gson.Gson;
+ import com.google.gson.JsonElement;
+ import com.google.gson.JsonObject;
+ */
+@WebServlet(urlPatterns = { "/Android/login" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String volunteerId;
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		response.setHeader("Cache-control", "no-cache, no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "-1");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		response.setHeader("Access-Control-Max-Age", "86400");
+
+		volunteerId = ((String) request.getParameter("username"));
+
+		String password = ((String) request.getParameter("password"));
+
+		GsonBuilder b = new GsonBuilder();
+		// b.registerTypeAdapterFactory(HibernateProxyTypeAdapter.FACTORY);
+		Gson gson = b.create();
+
+		Volunteer v;
+
+		v = VolunteerService.getVolunteerByName(volunteerId);
+
+		try {
+			if (v != null && v.getVolunteerPassword().equals(PasswordHash.getHash(password, v.getEmailAddress()))) {
+				HibernateUtil.clean(v);
+				out.println(gson.toJson(v));
+
+			}
+			else{
+				out.println("{}");
+			}
+		} catch (NoSuchAlgorithmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InvalidKeySpecException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		out.close();
+
+	}
+
+}
