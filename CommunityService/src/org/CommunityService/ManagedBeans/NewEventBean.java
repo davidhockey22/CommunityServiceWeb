808f85d1fc612ad14e9c4a9531d007b969c41a44
davidhockey22 <davidhockey22ster@gmail.com>
2014-04-20 23:43:18 -0400

    Added creator of event to event

--- CommunityService/src/org/CommunityService/ManagedBeans/NewEventBean.java
+++ CommunityService/src/org/CommunityService/ManagedBeans/NewEventBean.java
@@ -63,12 +63,13 @@ public class NewEventBean {
 	private DualListModel<String> skillModel = new DualListModel<String>();
 
 	List<Interest> allInterests = new ArrayList<Interest>();
-	List<Skill> allSkills = new ArrayList<Skill>();	
+	List<Skill> allSkills = new ArrayList<Skill>();
 
 	/**
-	 * Checks if the URL is valid and user is authorized to create an event. Initializes local variables on success.
-	 * Sends an HTTP Error code and returns on failure. Should be called only when generating the view for the first
-	 * time.
+	 * Checks if the URL is valid and user is authorized to create an event.
+	 * Initializes local variables on success. Sends an HTTP Error code and
+	 * returns on failure. Should be called only when generating the view for
+	 * the first time.
 	 * 
 	 * @throws IOException
 	 */
@@ -77,7 +78,8 @@ public class NewEventBean {
 
 		// check if user is authorized to create an event
 		if (!currentVolunteer.isLoggedIn()) {
-			// Throw an HTTP Response Error - Missing Credentials in case user is not logged in
+			// Throw an HTTP Response Error - Missing Credentials in case user
+			// is not logged in
 			context.getExternalContext().responseSendError(HttpServletResponse.SC_UNAUTHORIZED, "Not logged in");
 			context.responseComplete();
 			return;
@@ -88,14 +90,15 @@ public class NewEventBean {
 			orgId = Integer.parseInt(this.orgId);
 			this.currentVolunteer.refreshOrganizations();
 			if (!currentVolunteer.isMemberOfOrganizatoin(orgId)) {
-				// Throw an HTTP Response Error - Forbidden access in case user is not logged in
-				context.getExternalContext().responseSendError(HttpServletResponse.SC_FORBIDDEN,
-						"Not authorized to view page");
+				// Throw an HTTP Response Error - Forbidden access in case user
+				// is not logged in
+				context.getExternalContext().responseSendError(HttpServletResponse.SC_FORBIDDEN, "Not authorized to view page");
 				context.responseComplete();
 				return;
 			}
 		} catch (NumberFormatException e) {
-			// Throw an HTTP Response Error - Invalid Syntax in case invalid orgId was supplied
+			// Throw an HTTP Response Error - Invalid Syntax in case invalid
+			// orgId was supplied
 			context.getExternalContext().responseSendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid org id");
 			context.responseComplete();
 			return;
@@ -110,37 +113,35 @@ public class NewEventBean {
 		setSelectedOrgId(this.orgId);
 		// get cached organizations from currentVolunteer
 		organiztions = new TreeMap<String, String>();
-		for (OrganizationFollower organizationFollower : this.currentVolunteer.getVolunteer()
-				.getOrganizationFollowers()) {
+		for (OrganizationFollower organizationFollower : this.currentVolunteer.getVolunteer().getOrganizationFollowers()) {
 			if (organizationFollower.getAdmin() || organizationFollower.getMod()) {
-				organiztions.put(organizationFollower.getOrganization().getOrgName(), organizationFollower
-						.getOrganization().getOrgId().toString());
+				organiztions.put(organizationFollower.getOrganization().getOrgName(), organizationFollower.getOrganization().getOrgId().toString());
 			}
 		}
-		
+
 		initInterestAndSkills();
 	}
-	
+
 	void initInterestAndSkills() {
-		
-		//interests
+
+		// interests
 		List<Interest> interests = InterestService.getInterests();
 		allInterests = new ArrayList<Interest>();
-		for(Interest i : interests)
+		for (Interest i : interests)
 			allInterests.add(i);
-		
+
 		List<String> intStr = new ArrayList<String>();
-		for(Interest i : interests)
-			intStr.add(i.getName() + " - " + i.getDescription());		
-		
+		for (Interest i : interests)
+			intStr.add(i.getName() + " - " + i.getDescription());
+
 		interestModel.setSource(intStr);
-		
-		//skills
+
+		// skills
 		List<Skill> skills = null;
 		try {
 			skills = SkillService.getSkills();
 			allSkills = new ArrayList<Skill>();
-			for(Skill i : skills)
+			for (Skill i : skills)
 				allSkills.add(i);
 
 		} catch (Exception e) {
@@ -149,16 +150,17 @@ public class NewEventBean {
 		}
 
 		List<String> skStr = new ArrayList<String>();
-		for(Skill i : skills)
+		for (Skill i : skills)
 			skStr.add(i.getSkillName() + " - " + i.getDescription());
-				
+
 		skillModel.setSource(skStr);
 	}
 
 	public String Create() throws HibernateException {
-		// last check that user has permission, as organizationfollower info could be stale
-		OrganizationFollower orgF = OrganizationService.getOrganizationFollowerBy(Integer.parseInt(this.selectedOrgId),
-				this.currentVolunteer.getVolunteer().getVolunteerId());
+		// last check that user has permission, as organizationfollower info
+		// could be stale
+		OrganizationFollower orgF = OrganizationService.getOrganizationFollowerBy(Integer.parseInt(this.selectedOrgId), this.currentVolunteer.getVolunteer()
+				.getVolunteerId());
 
 		// create the event if member has permissions
 		if (orgF.getAdmin() || orgF.getMod()) {
@@ -166,43 +168,44 @@ public class NewEventBean {
 			event.setDescription(description);
 			event.setLocation(location);
 			event.setOrganization(orgF.getOrganization());
-			
+			event.setVolunteer(this.currentVolunteer.getVolunteer());
 			addInterestsAndSkills(event);
-			
+
 			EventService.addEvent(event);
 			this.eventId = event.getEventId().toString();
 		}
 
-		// redirect to same view either way, as the view recreation will trigger an orgfollower data refresh for the
+		// redirect to same view either way, as the view recreation will trigger
+		// an orgfollower data refresh for the
 		// logged in user
 		return "/NewEvent.xhtml?faces-redirect=true&includeViewParams=true";
 	}
-	
+
 	public void addInterestsAndSkills(Event ev) {
-		
+
 		Set<Interest> interestSet = new HashSet<Interest>();
 		String str = null;
-		for(String s : interestModel.getTarget()) {
-			for(Interest i : allInterests) {
-				str = new String( i.getName() + " - " + i.getDescription() );
-				if( s.equals( str ) ) {
-					interestSet.add( i );
+		for (String s : interestModel.getTarget()) {
+			for (Interest i : allInterests) {
+				str = new String(i.getName() + " - " + i.getDescription());
+				if (s.equals(str)) {
+					interestSet.add(i);
 				}
 			}
 		}
 		ev.setInterests(interestSet);
-		
+
 		Set<EventSkill> skillSet = new HashSet<EventSkill>();
-		for(String s : skillModel.getTarget()) {
-			for(Skill i : allSkills) {
-				str = new String( i.getSkillName() + " - " + i.getDescription() );
-				if( s.equals( str ) ) {
-					skillSet.add( new EventSkill( ev, i, false ) );
+		for (String s : skillModel.getTarget()) {
+			for (Skill i : allSkills) {
+				str = new String(i.getSkillName() + " - " + i.getDescription());
+				if (s.equals(str)) {
+					skillSet.add(new EventSkill(ev, i, false));
 				}
 			}
 		}
 		ev.setEventSkills(skillSet);
-	}	
+	}
 
 	public String getOrgId() {
 		return orgId;
@@ -295,7 +298,7 @@ public class NewEventBean {
 	public void setHoursBonus(float hoursBonus) {
 		this.hoursBonus = hoursBonus;
 	}
-	
+
 	public DualListModel<String> getInterestModel() {
 		return interestModel;
 	}
@@ -326,5 +329,5 @@ public class NewEventBean {
 
 	public void setAllSkills(List<Skill> allSkills) {
 		this.allSkills = allSkills;
-	}	
+	}
 }
